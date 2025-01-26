dd0924cf3a67584b3647fcf4c7215f618cb864cf
IgorKlevzhits <klevzhits94@icloud.com>
2025-01-26 16:51:58 +0300

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Игорь Клевжиц on 26.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
